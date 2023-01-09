464d7d1a8ff4902474c438f174a7a55a1fadbcb5
tatynaerkinbek <tatynaerkinbek@gmail.com>
2023-01-09 13:29:14 +0600

    start

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="header">
+                 <h1>Azim</h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
